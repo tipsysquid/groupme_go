35f02564266f6fa24a7bb5e3df86a9026ed6de93
EC2 Default User <ec2-user@ip-172-31-22-65.us-west-2.compute.internal>
2018-01-14 20:13:24 +0000

    work in progress, but currently deployed and operational

--- main.go
+++ main.go
@@ -2,78 +2,11 @@ package main
 
 import (
 	"fmt"
-	"encoding/json"
 	"log"
-	"io/ioutil"
+	"net/http"
+
+	"github.com/adammohammed/groupmebot"
 )
-
-type Bot struct {
-	id string `json:"bot_id"`
-	groupID	string `json:"group_id"`
-	host	string	`json:"host"`
-	port	string	`json:"port"`
-	logfile	string	`json:"logfile"`
-	server	string
-	Hooks	map[string]func(InMsg) string
-}
-
-type InMsg struct {
-	avatar_url string `json:"avatar_url"`
-	id	string	`json:"id"`
-	name	string	`json:"name"`
-	sender_id	string	`json:"sender_id"`
-	sender_type	string	`json:"sender_type"`
-	system	bool	`json:"system"`
-	text	string	`json:"text"`
-	user_id	string	`json:"user_id"`
-}
-
-type OutMsg struct {
-	id	string	`json:"bot_id"`
-	text	string	`json:"text"`
-}
-
-// Parses JSON and creates bot
-func CreateJsonBot(filename string) (*Bot, error) {
-	file, err := ioutil.ReadFile(filename)
-
-	var bot Bot
-	if err != nil {
-		log.Fatal("Unable to parse bot config")
-		return nil, err
-	}
-
-	json.Unmarshal(file, &bot)
-
-	bot.server = bot.host + ":" + bot.port
-	log.Printf("Creating bot at %s\nLogging at %s\n", bot.server, bot.logfile)
-	bot.Hooks = make(map[string]func(InMsg) string)
-
-	log.Println("Bot Created:"+bot.id)
-	return &bot, err
-
-}
-
-func HelloTest(str string) {
-	fmt.Println(str)
-}
-
-func main() {
-        fmt.Println("hello world!")
-	bot, err := CreateJsonBot("bot_config.json")
-	if err != nil {
-		log.Fatal("Could not create bot from JSON")
-	}
-
-	log.Println(bot.server)
-	bot.AddHook("Hi!$", hello)
-	bot.AddHook("Hello!$", hello2)
-	bot.AddHook("test$", test)
-	bot.AddHook("request", suggestion)
-	log.Printf("Listening on %v...\n", bot.server)
-	http.HandleFunc("/", bot.Handler())
-	log.Fatal(http.ListenAndServe(bot.server, nil))
-}
 
 /*
  Test hook functions
@@ -82,25 +15,53 @@ func main() {
  Hooks will be traversed until match occurs
 */
 func hello(msg groupmebot.InboundMessage) (string) {
-        resp := fmt.Sprintf("Hi, %v.", msg.Name)
-        return resp
+	resp := fmt.Sprintf("Hi, %v.", msg.Name)
+	return resp
 }
 
 func hello2(msg groupmebot.InboundMessage) (string) {
-        resp := fmt.Sprintf("Hello, %v.", msg.Name)
-        return resp
+	resp := fmt.Sprintf("Hello, %v.", msg.Name)
+	return resp
 }
 
 func test(msg groupmebot.InboundMessage) (string) {
-        resp := fmt.Sprintf("This bot is a work in prorgress, %v.", msg.Name)
-        return resp
+	resp := fmt.Sprintf("This bot is a work in prorgress, %v.", msg.Name)
+	return resp
 }
 
 func suggestion(msg groupmebot.InboundMessage) (string) {
-        resp := fmt.Sprintf("Your suggestion has been saved, %v.", msg.Name)
+	resp := fmt.Sprintf("Your suggestion has been saved, %v.", msg.Name)
         log.Printf("Suggestion from %s: msg %s\n", msg.Name, msg)
-        return resp
+	return resp
 }
 
+func help(msg groupmebot.InboundMessage) (string) {
+	resp := fmt.Sprintf("Available commands: request\nType 'request' followed by an idea, and it will be saved for later")
+	return resp
+}
 
+func main() {
 
+	bot, err := groupmebot.NewBotFromJson("mybot_cfg.json")
+	if err != nil {
+		log.Fatal("Could not create bot structure")
+	}
+
+	// Make a list of functions
+	bot.AddHook("Hi!$", hello)
+	bot.AddHook("Hello!$", hello2)
+	bot.AddHook("test$",test)
+	bot.AddHook("request",suggestion)
+	bot.AddHook("Request",suggestion)
+	bot.AddHook("/help",help)
+	// Create Server to listen for incoming POST from GroupMe
+	log.Printf("Listening on %v...\n", bot.Server)
+	http.HandleFunc("/", bot.Handler())
+	initMsg(bot)
+	log.Fatal(http.ListenAndServe(bot.Server, nil))
+}
+
+func initMsg (bot *groupmebot.GroupMeBot){
+	bot.SendMessage("System warming up...\nType /help for available commands")
+	bot.SendMessage("Now supporting lazy-patch for uppercase R!")
+}
